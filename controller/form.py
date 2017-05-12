753b00aeb0cd35c556bdcd135aeec1fd6d2b83d7
tiantao <zhangtiantao@youmi.net>
2017-05-12 18:34:50 +0800

    ..

--- controller/form.py
+++ controller/form.py
@@ -1,8 +1,9 @@
 from flask_wtf import  Form
-from wtforms import TextField, BooleanField, PasswordField
-from wtforms.validators import Required
+from wtforms import TextField, BooleanField, PasswordField, SubmitField
+from wtforms.validators import Required, Email, Length
 
 class LoginForm(Form):
-    name = TextField('name', validators=[Required()])
+    name = TextField('name', validators=[Required(), Length(max=15)])
     password = PasswordField('password', validators=[Required()])
     remember_me = BooleanField('Remember_me', default=False)
+    submit = SubmitField('Log in')
